a86da6c544f49942295f052efede443c6dba33b9
José Caldas <jrcaldas@yahoo.com.br>
2023-04-05 17:32:52 -0300

    feat: add nodemon and morgan

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const productRoute = require("./routes/products");
 const orderRoute = require("./routes/order");
 
+app.use(morgan("dev"));
+
 app.use("/products", productRoute);
 app.use("/pedido", orderRoute);
+
+// Caso não encontre nenhuma rota
+app.use((req, res, next) => {
+  const error = new Error("Rota não encontrada");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
