3ceb87a36c7bcc82fea1a1917f32dbaf38d7ce0c
José Caldas <jrcaldas@yahoo.com.br>
2023-04-10 17:11:15 -0300

    refactor: folder structure

--- app.js
+++ app.js
@@ -1,46 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const morgan = require("morgan");
-const bodyParcer = require("body-parser");
 
-const productRoute = require("./routes/products");
-const orderRoute = require("./routes/order");
-
-app.use(morgan("dev"));
-app.use(bodyParcer.urlencoded({ extended: false })); // Apenas dados simples
-app.use(bodyParcer.json()); // Apenas formato json de entrada no body
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Header",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    return res.status(200).send({});
-  }
-
-  next();
+app.use(cors());
+app.use(express.json());
+app.listen(3000, function () {
+  console.log("Servidor Online!");
 });
-
-app.use("/produtos", productRoute);
-app.use("/pedidos", orderRoute);
-
-// Caso não encontre nenhuma rota
-app.use((req, res, next) => {
-  const error = new Error("Rota não encontrada");
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  return res.send({
-    error: {
-      message: error.message,
-    },
-  });
-});
-
-module.exports = app;
